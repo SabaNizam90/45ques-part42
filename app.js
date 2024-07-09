c4c1c0d9c4326e358845fe959df2409930c22655
Saba Nizam <160767347+SabaNizam90@users.noreply.github.com>
2024-07-10 00:35:03 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+// 42. Great Magicians: Start with a copy of your program from Exercise 39.
+// Write a function called make_great() that modifies the array of magicians by adding
+// the phrase the Great to each magician’s name. Call show_magicians() to
+// see that the list has actually been modified.
+function show_magicians(magicians) {
+    magicians.forEach(function (name) { return console.log(name); });
+}
+function make_great(magicians) {
+    return magicians.map(function (name) { return "The Great ".concat(name); });
+}
+var magicians_names = ["Ali", "Salman", "Asif"];
+var great_magicians = make_great(magicians_names);
+//console.log(great_magicians);
+show_magicians(great_magicians);
